b6a6747b01b8a80733d2337a9cea7d63bff1cd93
Ka Hou, CHOI <kz1412@link.cuhk.edu.hk>
2022-06-04 01:16:44 +0800

    feat: wake server when first landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,24 @@
 import { RoutesPath } from './components/RoutesPath/RoutesPath';
+import { useEffect } from 'react';
+import { AxiosInstance } from '../../utils/AxiosInstance';
 
 const App = () => {
+
+	// Initialize: wake server
+	const init = async () => {
+		try {
+			const result = await AxiosInstance.get('/');
+			return result;
+		} catch (error) {
+			console.log(error);
+			throw error;
+		}
+	};
+
+	useEffect(() => {
+		init();
+	}, []);
+
 	return <RoutesPath />;
 };
 
